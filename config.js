a43d9ab4a19baa9bb73b7295d9906096f84c457e
thennakoonvipula666-oss <thennakoonvipula666@gmail.com>
2026-09-18 15:39:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "sNcGhSwJ#wdhsGhYRzZ_g9y04nmkdX8nCq4N_k3JkWnQV2DRH7Lw",
+SESSION_ID: process.env.SESSION_ID || "FENCATZY#_440NwCqJ4W3hlghCkFdBZD8ucG4JOrljyZd887QWnY",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
